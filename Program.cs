f8a247d13c03ea4c8be6aa6934f987a1708a860a
ryan-BWCLE <70779371+ryan-BWCLE@users.noreply.github.com>
2020-12-01 12:31:19 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using Capstone.Classes;
+using Capstone.UI;
+using MenuFramework;
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Capstone
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            VMInventoryLoader vMInventoryLoader = new VMInventoryLoader();
+            vMInventoryLoader.StockProducts();
+
+            IEnumerable<Product> products = vMInventoryLoader.StockProducts();
+
+            VendingMachine vendingMachine = new VendingMachine(products);
+
+            MainMenu mainMenu = new MainMenu(vendingMachine);
+
+            
+        }
+
+    }
+}
